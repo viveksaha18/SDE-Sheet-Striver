4e04cda314d21444a05fce935472751f79ee692a
Vivek18 <viveksaha096@gmail.com>
2025-12-21 22:14:04 +0530

    Optimal Approach Done!

--- Recursion/subset2.cpp
+++ Recursion/subset2.cpp
@@ -26,11 +26,49 @@ void f(int currIdx, vector<int>& temp, vector<int>& arr) {
         return ans;
     }
 };
+
+// Optimized Approach Without Using Set
+class Solution1 {
+public:
+void f(int currIdx, vector<int>& temp, vector<vector<int>>& ans, vector<int>& nums) {
+    if(currIdx == nums.size()) {
+        ans.push_back(temp);
+        return;
+    }
+    // include
+    temp.push_back(nums[currIdx]);
+    f(currIdx+1, temp, ans, nums);
+    temp.pop_back(); // backtrack
+    int idx = currIdx+1;
+    while(idx < nums.size() && nums[idx] == nums[idx-1]) {
+        idx++;
+    }
+    f(idx, temp, ans, nums);
+}
+    vector<vector<int>> subsetsWithDup(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+        vector<int> temp;
+        vector<vector<int>> ans;
+        f(0, temp, ans, nums);
+        return ans;
+    }
+};
 int main() {
     Solution obj;
     vector<int> arr = {1,2,2};
     vector<vector<int>> result = obj.subsetsWithDup(arr);
     for (const auto& subset : result) {
+        cout << "[";
+        for (size_t i = 0; i < subset.size(); ++i) {
+            cout << subset[i];
+            if (i < subset.size() - 1) cout << ", ";
+        }
+        cout << "]\n";
+    }
+
+    Solution1 obj1;
+    vector<vector<int>> result1 = obj1.subsetsWithDup(arr);
+    for (const auto& subset : result1) {
         cout << "[";
         for (size_t i = 0; i < subset.size(); ++i) {
             cout << subset[i];
