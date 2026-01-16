0408c821746ff9f243a1767799a43c17de13304b
Vivek18 <viveksaha096@gmail.com>
2026-01-16 20:14:10 +0530

    Optimal Solution using Moore's Voting Algorithm (Majority Element Problem)!

--- Array Part3/majorityElement.cpp
+++ Array Part3/majorityElement.cpp
@@ -13,9 +13,32 @@ public:
         return 0;
     }
 };
+// Optimal Approach using Moore's Voting Algorithm
+// Time Complexity: O(n)
+// Space Complexity: O(1)
+class Solution1 {
+public:
+    int majorityElement(vector<int>& nums) {
+        int el;
+        int cnt = 0;
+        int n = nums.size();
+        for(int i = 0; i < n; i++) {
+            if(cnt == 0) {
+                cnt=1;
+                el = nums[i];
+            }
+            else if(el == nums[i]) cnt++;
+            else cnt--;
+        }
+        return el;
+    }
+};
 int main() {
     Solution s;
     vector<int> nums = {3,2,3};
     cout << s.majorityElement(nums) << endl;
+    Solution s1;
+    vector<int> nums1 = {2,2,1,1,1,2,2};
+    cout << s1.majorityElement(nums1) << endl;
     return 0;
 }
